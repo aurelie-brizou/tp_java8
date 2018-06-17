acf7d4e36fd3d08c70ad2f9fcf11a7f188fe7a2d
Aurelie <brizouaurelie@gmail.com>
2018-06-17 18:32:57 +0200

    exo 5 complété et tests ok

--- src/test/java/java8/ex05/Function_05_Test.java
+++ src/test/java/java8/ex05/Function_05_Test.java
@@ -39,9 +39,5 @@ public class Function_05_Test {
 		// remplace la boucle for par l'invocation de la méthode forEach
 		// utilise la méthode andThen pour chaîner les vérifications verifyAge et verifyPassword
 		personList.forEach((verifyAge).andThen(verifyPassword));
-		for(Person p : personList) {
-			verifyAge.accept(p);
-			verifyPassword.accept(p);
-		}
 	}
 }
