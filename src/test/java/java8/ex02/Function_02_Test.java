763b1cfdbd8c83e9cb558d412902bdb8d47adc9b
Aurelie <brizouaurelie@gmail.com>
2018-06-17 17:40:00 +0200

    exo 2 complété et tests ok

--- src/test/java/java8/ex02/Function_02_Test.java
+++ src/test/java/java8/ex02/Function_02_Test.java
@@ -11,24 +11,26 @@ import java.util.function.BiFunction;
  */
 public class Function_02_Test {
 
-    //  tag::buildAccount[]
-    // TODO Compléter la fonction buildAccount
-    // TODO la fonction possède 2 paramètres en entrée : une personne et un solde
-    BiFunction<Person, Integer, Account> buildAccount = null;
-    //  end::buildAccount[]
+	//  tag::buildAccount[]
+	
+	// Compléter la fonction buildAccount
+	// la fonction possède 2 paramètres en entrée : une personne et un solde
+	BiFunction<Person, Integer, Account> buildAccount = (p, i) -> new Account(p, i);
+	
+	//  end::buildAccount[]
 
-    @Test
-    public void test_build_account() throws Exception {
+	@Test
+	public void test_build_account() throws Exception {
 
-        // TODO invoquer la fonction buildAccount pour que le test soit passant
-        Account account = null;
+		// invoque la fonction buildAccount pour que le test soit passant
+		Account account = buildAccount.apply(new Person("John", "France", 80, "pass"), 500);
 
-        assert account.getBalance().equals(500);
-        assert account.getOwner().getFirstname().equals("John");
-        assert account.getOwner().getLastname().equals("France");
-        assert account.getOwner().getAge().equals(80);
-        assert account.getOwner().getPassword().equals("pass");
-    }
+		assert account.getBalance().equals(500);
+		assert account.getOwner().getFirstname().equals("John");
+		assert account.getOwner().getLastname().equals("France");
+		assert account.getOwner().getAge().equals(80);
+		assert account.getOwner().getPassword().equals("pass");
+	}
 
 
 }
