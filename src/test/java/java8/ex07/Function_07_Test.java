8287d390ee0ba35280d9638489d4be54e2e0a16b
Aurelie <brizouaurelie@gmail.com>
2018-06-17 19:04:55 +0200

    exo 7 complété et tests ok

--- src/test/java/java8/ex07/Function_07_Test.java
+++ src/test/java/java8/ex07/Function_07_Test.java
@@ -10,16 +10,21 @@ import java.util.function.IntBinaryOperator;
 public class Function_07_Test {
 
     // tag::format[]
-    // TODO compléter la méthode pour qu'elle renvoie une chaîne de caractères de la forme "(<nb1><symbol><nb2>)=<resultat>"
-    // TODO ex. "(10+11)=21", "(5-2)=3"
+
+    /**
+     * @param nb1
+     * @param nb2
+     * @param symbol
+     * @param operator
+     * @return une chaîne de caractères de la forme "(<nb1><symbol><nb2>)=<resultat>" ex:"(10+11)=21"
+     */
     String format(int nb1, int nb2, String symbol, IntBinaryOperator operator) {
-        // TODO
-        return null;
+        return "("+ nb1 + symbol + nb2 + ")=" + operator.applyAsInt(nb1, nb2);
     }
     // end::format[]
 
-    // TODO définir sum pour que le test test_format_sum() soit passant
-    IntBinaryOperator sum = null;
+    // définit sum pour que le test test_format_sum() soit passant
+    IntBinaryOperator sum = (nb1, nb2) -> nb1 + nb2;
 
     @Test
     public void test_format_sum() throws Exception {
@@ -29,8 +34,8 @@ public class Function_07_Test {
         assert result.equals("(12+13)=25");
     }
 
-    // TODO définir substract afin que le test test_format_subtract() soit passant
-    IntBinaryOperator substract = null;
+    // TODO définit substract afin que le test test_format_subtract() soit passant
+    IntBinaryOperator substract = (nb1, nb2) -> nb1 - nb2;
 
     @Test
     public void test_format_subtract() throws Exception {
@@ -40,6 +45,3 @@ public class Function_07_Test {
         assert result.equals("(2-3)=-1");
     }
 }
-
-
-
